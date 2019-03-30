8d45eea334b0f0940b0602038a6b476e48b8d774
jeremyb31 <jeremy53561@gmail.com>
2019-03-30 10:26:26 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install dkms git build-essential
+git clone -b extended https://github.com/lwfinger/rtlwifi_new.git
+sudo dkms add ./rtlwifi_new
+sudo dkms install rtlwifi-new/0.6
+sudo cp -r rtlwifi_new/firmware/rtlwifi/ /lib/firmware/rtlwifi/
+echo "options rtl8723de ant_sel=2" | sudo tee /etc/modprobe.d/rtl8723de.conf
+echo "options rtl8723be ant_sel=2" | sudo tee /etc/modprobe.d/rtl8723be.conf
